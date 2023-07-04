23f9130eac5d8ae197fc50c35196d2cadec8705c
Dzaky <muhadrian061@gmail.com>
2023-07-04 22:55:48 +0700

    con

---
+++ Rongsokin/Rongsokin/PROJEK CODECRAFTERS/src/Controller/DaftarController.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class DaftarController implements Initializable{
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private TextField tfPass;
+
+    @FXML
+    private TextField tfUname;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+}
